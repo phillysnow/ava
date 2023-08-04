c30318be99acc79d3ca82f4e70de78c92ca19248
Phil Snow <phil.snow@prismic.io>
2023-08-04 23:07:34 +0200

    first commit

---
+++ content/_index.md
@@ -0,0 +1,23 @@
+---
+banner:
+  title: AVA Carpentry Inc
+  content: We have a strong reputation for delivering high quality work on budget and on time. Contact us at the details below.
+  image: /images/Carpenter-Vector-Illustration.jpg
+  button:
+    label: "Contact Us"
+    link: "/contact"
+
+# feature
+feature: 
+  title: Contact Us
+  features:
+  - name: "Phone"
+    icon: "/images/user-clock.svg"
+    content: "914 309 2069"
+  - name: "Email"
+    icon: "/images/love.svg"
+    content: "avacarpentryinc@gmail.com"
+  - name: "Address"
+    icon: "/images/speedometer.svg"
+    content: "64-44 58th road, Maspeth, Queens, NY 11378"
+---
---
+++ layouts/Baseof.js
@@ -0,0 +1,72 @@
+import config from "@config/config.json";
+import { plainify } from "@lib/utils/textConverter";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const Base = ({
+  title,
+  meta_title,
+  description,
+  image,
+  noindex,
+  canonical,
+  children,
+}) => {
+  const { meta_image, meta_description } = config.metadata;
+  const { base_url } = config.site;
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        {/* title */}
+        <title>
+          {plainify(
+            meta_title ? meta_title : title ? title : config.site.title
+          )}
+        </title>
+
+        {/* canonical url */}
+        {canonical && <link rel="canonical" href={canonical} itemProp="url" />}
+
+        {/* noindex robots */}
+        {noindex && <meta name="robots" content="noindex,nofollow" />}
+
+        {/* meta-description */}
+        <meta
+          name="description"
+          content={plainify(description ? description : meta_description)}
+        />
+
+        {/* og-title */}
+        <meta
+          property="og:title"
+          content={plainify(
+            meta_title ? meta_title : title ? title : config.site.title
+          )}
+        />
+
+        {/* og-description */}
+        <meta
+          property="og:description"
+          content={plainify(description ? description : meta_description)}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`${base_url}/${router.asPath.replace("/", "")}`}
+        />
+
+        {/* og-image */}
+        <meta
+          property="og:image"
+          content={`${base_url}${image ? image : meta_image}`}
+        />
+      </Head>
+      {/* main site */}
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Base;
